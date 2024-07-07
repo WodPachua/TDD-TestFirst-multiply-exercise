4ad7f3e3472818364f339b315833aaa00c03718e
wodpachua <molarag21996@gmail.com>
2024-07-07 10:14:33 +0300

    Test First TDD Cycle 1

--- test/test.js
+++ test/test.js
@@ -1,4 +1,8 @@
 import assert from "assert";
-import { multiply } from "../multiply";
+import { multiply } from "../multiply.js";
 
-describe("multiply exercise", function () {});
+describe("multiply exercise", function () {
+  it("Should return correct product of numbers - CYCLE 1", function () {
+    assert.equal(multiply(1, 1), 1);
+  });
+});
